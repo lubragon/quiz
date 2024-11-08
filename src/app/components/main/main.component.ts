d83c3ef133db4330217936c26f3309530aaffb54
Lucas Braganca Goncalves (Holding DTI) <lucasbg@aguiabranca.com.br>
2024-11-08 15:21:56 -0300

    Alterado main component

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-main', 
   templateUrl: './main.component.html', 
-  styleUrls: ['./main.component.scss'] 
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
   constructor(private router: Router){
