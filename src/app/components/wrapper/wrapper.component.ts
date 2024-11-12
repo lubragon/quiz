993603a93e3e964e08ef62cf6b787ee4de934890
Lucas Braganca Goncalves (Holding DTI) <lucasbg@aguiabranca.com.br>
2024-11-12 15:11:39 -0300

    Alterado wrapper

---
+++ src/app/components/wrapper/wrapper.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-wrapper', 
+  templateUrl: './wrapper.component.html', 
+  styleUrl: './wrapper.component.scss',
+})
+export class WrapperComponent {
+  constructor(private router: Router){
+
+  }
+  
+
+  goToQuiz() {
+    this.router.navigate(['/quiz']);
+  }
+
+
+}
