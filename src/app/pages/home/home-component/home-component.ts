68ff3a8a05aacac33cb34f2a0883e9ed65c2aa7a
Lucas Braganca Goncalves (Holding DTI) <lucasbg@aguiabranca.com.br>
2024-11-12 15:42:44 -0300

    Adicionado funcionalidade de navegação para o jogo na home

--- src/app/pages/home/home-component/home-component.ts
+++ src/app/pages/home/home-component/home-component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home-component',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './home-component.scss'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
+
+  navigateToQuiz(){
+    this.router.navigate(['/quiz/menu'])
+  }
 
 }
