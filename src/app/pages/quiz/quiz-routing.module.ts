a08be45a491180f9ace1e5db505c3f549a6c78ae
Lucas Braganca Goncalves (Holding DTI) <lucasbg@aguiabranca.com.br>
2024-11-14 15:55:06 -0300

    Add rota pra quiz selecionar

--- src/app/pages/quiz/quiz-routing.module.ts
+++ src/app/pages/quiz/quiz-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; 
 import { QuizMenuComponent } from './quiz-menu/quiz-menu.component';
 import { QuizGameComponent } from './quiz-game/quiz-game.component';
+import { QuizSeletorComponent } from './quiz-seletor/quiz-seletor.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'game',
     component: QuizGameComponent
+  },
+  {
+    path: 'selecionar',
+    component: QuizSeletorComponent
   }
 
 ]
