66948e7f0162bf48b420a043209e069f4e174fc5
Lucas Braganca Goncalves (Holding DTI) <lucasbg@aguiabranca.com.br>
2024-11-14 14:39:48 -0300

    Alteracoes significativas em quiz respotas.

--- src/app/pages/quiz/quiz-respostas/quiz-respostas.component.ts
+++ src/app/pages/quiz/quiz-respostas/quiz-respostas.component.ts
@@ -1,24 +1,56 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { QuizGameService } from '../../../services/quizGame/quizGame.service';
-import { Pergunta } from '../../../types/interfaces.types';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Pergunta, PossiveisRespostas } from '../../../types/interfaces.types';
+import { OnChangeType } from 'ng-zorro-antd/core/types';
 
 @Component({
   selector: 'app-quiz-respostas',
   templateUrl: './quiz-respostas.component.html',
   styleUrl: './quiz-respostas.component.scss'
 })
-export class QuizRespostasComponent{
+export class QuizRespostasComponent implements OnChanges
+{
+
   
-  @Input() respostas: {texto: string; isCorrect: boolean}[] = [];
+  @Input() respostas: PossiveisRespostas[] = [];
   @Input() isClicked: boolean = false;
   @Input() perguntaAtual: Pergunta  | undefined;
 
 
-  @Output() responder = new EventEmitter<{texto: string; isCorrect: boolean}>;
+  @Output() respostaOutput = new EventEmitter<PossiveisRespostas>;
 
-  onResponder(resposta: {texto: string; isCorrect: boolean})
-  {
-    this.responder.emit(resposta)
+
+  respostaSelecionada: PossiveisRespostas | null = null;
+
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const checarMudanca = changes['perguntaAtual']?.currentValue
+    if(checarMudanca){
+      this.respostaSelecionada = null;
+    }
   }
+
+  onResponder(resposta: PossiveisRespostas)
+  {
+    this.respostaSelecionada = resposta;
+    this.respostaOutput.emit(resposta)
+  }
+
+  getButtonClass(resposta: PossiveisRespostas): string {
+    // Define todos os botões com style padrão caso nenhuma resposta seja selecionada
+    if (!this.respostaSelecionada) {
+      return 'respostaButton';
+    }
+  
+    // Define style da resposta que foi selecionada
+    if (this.respostaSelecionada === resposta) {
+      return this.respostaSelecionada.isCorrect ? 'respostaButton correta' : 'respostaButton incorreta';
+    }
+  
+    // Define style das respostas que não foram selecionadas
+    return resposta.isCorrect ? 'respostaButton correta' : 'respostaButton';
+  }
+
+
 
 }
